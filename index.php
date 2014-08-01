c8210d2505653ccf352132ea37be829b7b171e52
AurumGallente <aurum.gallente@gmail.com>
2014-08-01 17:03:01 +0300

    some changes

--- index.php
+++ index.php
@@ -7,8 +7,15 @@
     <body>
         <ul>            
         <?php foreach($users as $user){ ?>
-            <li><?= $user['name'] ?> <?= $user['lastname'] ?> - <?= $user['position'] ?></li>
+            <li id="<?= $user['id'] ?>"> <?= $user['name'] ?> <?= $user['lastname'] ?> - <?= $user['position'] ?></li>
         <?php } ?>            
         </ul>
+        
+        
+        <div class="pearson">
+            <p id="name"> </p>
+            <p id="lastname"> </p>
+            <p id="position"> </p>
+        </div>
     </body>
 </html> 
